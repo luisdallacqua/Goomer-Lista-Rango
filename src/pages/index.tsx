bde216f8f1decee878ca947247de5419708a1dda
luisdallacqua <luisdallacqua@gmail.com>
2022-01-17 14:34:09 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+
+export default function Home() {
+  return (
+    <Container>
+      <Typography variant="h5" component="h1" align="center">
+        Welcome to boilerplate for NextJs + Mui v5 + React Testing Library +
+        Eslint and prettier
+      </Typography>
+    </Container>
+  )
+}
